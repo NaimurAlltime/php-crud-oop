87569a2464b58b636019a489dee57dcd64ef68b9
Naimur Rahman <naimur2231@gmail.com>
2024-11-27 02:09:55 +0600

    database connection done

---
+++ db_connect.php
@@ -0,0 +1,29 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'demo');
+
+class dbConnect
+{
+    //constructor
+function __construct()
+{
+$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+$this->dbh=$conn;
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+}
+
+
+public function insert($fname,$email,$contact,$gender,$education,$adrss)
+{
+$add=mysqli_query($this->dbh,"INSERT INTO user1(username,email,phone,gender,education,addrss) 
+values('$fname','$email','$contact','$gender','$education','$adrss')");
+return $add;
+}
+}
+?>
